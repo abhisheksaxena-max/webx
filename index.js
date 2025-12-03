62ad5eab042e693529b0856e8ac3c2b77a3ddeba
abhisheksaxena-max <abhishek.saxena@lmsathena.com>
2025-12-03 10:50:42 +0530

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const zoomRoute = require('./routes/zoomRoute')
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
