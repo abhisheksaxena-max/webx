1658b04e8a607c357c45bb6a3216d93705027c08
abhisheksaxena-max <abhishek.saxena@lmsathena.com>
2025-12-03 10:30:23 +0530

    test

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
 const express = require("express");
 require("dotenv").config();
-const zoomRoute = require('./routes/zoomRoute')
+// const zoomRoute = require('./routes/zoomRoute')
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
+
 app.get('/', (req,res)=>{
   console.log('welcome to webx');
-  
 })
-// 1 Redirect user to Zoom OAuth to authorize app
-// app.get("",);
 
-// // 2 Handle OAuth callback and exchange code for access token
-// app.get("/zoom/oauth/callback",);
-
-// // 3 Refresh access token
-// app.get("/zoom/refresh",);
-
-// // 4 register a webinar
-// app.post("/zoom/registar-webinar", );
-
-// app.get("/zoom/participants",  );
-
-// app.get("/api/webinar/merged-report", );
-
-app.use('/zoom', zoomRoute)
+// app.use('/zoom', zoomRoute)
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
